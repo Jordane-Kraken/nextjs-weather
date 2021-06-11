189862863d8232cf1349f4e361a913939dad28ce
Jordane-Kraken <67547999+Jordane-Kraken@users.noreply.github.com>
2021-06-11 13:57:40 +0200

    Delete next.config.js

--- weather-app/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    API_KEY : '4e35873cc5245dd107fe6db20da47da8',
-    API_KEY_HERE : 'Op3BM3dw17G-oSI-afo5P-NXG1Py2eJHQOk6MFPQ60M',
-    APP_ID_HERE : 'u9hbX3kVUTlY03W2Enlc',
-  },
-}
